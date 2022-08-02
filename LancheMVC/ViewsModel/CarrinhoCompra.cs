e432544736f9044d097e52e63e62fe0c4e255679
Felipealvesnn <felipe.alvesnn@gmail.com>
2022-08-02 08:16:07 -0300

    att

---
+++ LancheMVC/ViewsModel/CarrinhoCompra.cs
@@ -0,0 +1,42 @@
+﻿using LancheMVC_Data.Contexto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LancheMVC_Domain
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _Context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _Context = context;
+        }
+
+        public string CarrinhocompraID { get; set; }
+        public List< CarrinhoCompraItem> CarrinhoCompraItems{ get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider Services) {
+            //define uma sessão
+            ISession session =
+                Services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //obtem um serviço do tipo do nosso contexto 
+            var context = Services.GetService<AppDbContext>();
+
+            //obtem ou gera o Id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            ////atribui o id do carrinho na Sessão
+            //session.SetString("CarrinhoId", carrinhoId);
+
+            //retorna o carrinho com o contexto e o Id atribuido ou obtido
+            return new CarrinhoCompra(context)
+            {
+                CarrinhocompraID = carrinhoId
+            };
+
+        }
+    }
+}
