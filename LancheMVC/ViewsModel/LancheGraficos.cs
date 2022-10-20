b78bbd31c8a4efc427abe2d7888470de26c524b9
Felipe NoWork <felipe.rudra@hotmail.com>
2022-10-20 14:08:55 -0300

    At menu ADM

---
+++ LancheMVC/ViewsModel/LancheGraficos.cs
@@ -0,0 +1,9 @@
+﻿namespace LancheMVC.ViewsModel
+{
+    public class LancheGraficos
+    {
+        public string LancheNome { get; set; }
+        public int LanchesQuantidade { get; set; }
+        public decimal LanchesValorTotal { get; set; }
+    }
+}
