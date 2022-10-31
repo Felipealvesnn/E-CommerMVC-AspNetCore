549c573a27b4031f2c6c54cbf9d4f6f4f7a23186
Felipe NoWork <felipe.rudra@hotmail.com>
2022-10-31 14:09:39 -0300

    Mudanças no navbar Admin e classe /controler grafico vendas criado e add pacote do tema  admin

---
+++ LancheMVC/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -0,0 +1,41 @@
+﻿using LancheMVC.ViewsModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LancheMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize("Admin")]
+    public class AdminGraficoController : Controller
+    {
+
+        private readonly GraficoVendasServices _graficoVendasServices;
+
+        public AdminGraficoController(GraficoVendasServices graficoVendasServices)
+        {
+            _graficoVendasServices = graficoVendasServices ?? throw new ArgumentNullException(nameof(graficoVendasServices));
+        }
+
+        public JsonResult VendasLanches(int dias)
+        {
+            var lanchesVendasTotais = _graficoVendasServices.GetVendasLanches(dias);
+            return Json(lanchesVendasTotais);
+        }
+        [HttpGet]
+        public IActionResult Index(int dias)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult VendasMensal(int dias)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult VendasSemanal(int dias)
+        {
+            return View();
+        }
+    }
+}
